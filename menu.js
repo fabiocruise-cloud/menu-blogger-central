542bdb1587a57a8e3032b1245f482c0cb6d46fb2
fabiocruise-cloud <fabiocruise@gmail.com>
2025-11-23 06:18:16 -0300

    Update menu.js
    financeiro

--- menu.js
+++ menu.js
@@ -2,5 +2,6 @@ window.BLOGGER_CENTRAL_MENU = [
  {"title": "Fábio Cruise - Áreas da Vida - Principal", "url": "https://areas-da-vida.blogspot.com/" },
  {"title": "1 🙏🛐😇 FÉ ORAÇÃO VIDA ETERNA", "url": "https://fe-oracao-e-vida-eterna.blogspot.com/" },
  {"title": "2 ⛹️🚴 BEM ESTAR & SAÚDE 🏋️", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com/" },
- {"title": "3 👨‍💼💼 CARREIRA+PROFISSÃO", "url": "https://fabiocruise-trabalho.blogspot.com/" }
+ {"title": "3 👨‍💼💼 CARREIRA+PROFISSÃO", "url": "https://fabiocruise-trabalho.blogspot.com/" },
+ {"title": "4 💲💳 FINANÇAS & INVESTIMENTOS", "url": "https://investimentos-financeiros.blogspot.com/" }
 ];
