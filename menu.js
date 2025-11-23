dcceb247c2842ce24a61bb3022ab9742e4beb92e
fabiocruise-cloud <fabiocruise@gmail.com>
2025-11-23 03:50:16 -0300

    Update URL for Áreas da Vida menu item

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
 window.BLOGGER_CENTRAL_MENU = [
-  { "title": "Fábio Cruise - Áreas da Vida - Principal", "url": "https://areas-da-minha-vida.blogspot.com/" },
+  { "title": "Fábio Cruise - Áreas da Vida - Principal", "url": "https://areas-da-vida.blogspot.com/" },
   { "title": "1 🙏🛐😇 FÉ ORAÇÃO VIDA ETERNA", "url": "https://fe-oracao-e-vida-eterna.blogspot.com/" }
 ];
