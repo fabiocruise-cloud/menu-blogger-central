1cd0f22126f62c0ea36400592dee6fb7e85f6af1
fabiocruise-cloud <fabiocruise@gmail.com>
2025-11-23 04:58:09 -0300

    Update menu URLs for Blogger Central
    EXCEL AUTOMAÇÃO 2

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
 window.BLOGGER_CENTRAL_MENU = [
- {"title": "Fábio Cruise - Áreas da Vida - Principal", "url": "https://areas-da-vida.blogspot.com/" },
- {"title": "1 🙏🛐😇 FÉ ORAÇÃO VIDA ETERNA", "url": "https://areas-da-vida.blogspot.com/" },
- {"title": "2 ⛹️🚴 BEM ESTAR & SAÚDE 🏋️", "url": "https://areas-da-vida.blogspot.com/" }
+ {"title": "Fábio Cruise - Áreas da Vida - Principal", "url": "https://areas-da-minha-vida.blogspot.com/" },
+ {"title": "1 🙏🛐😇 FÉ ORAÇÃO VIDA ETERNA", "url": "https://fe-oracao-e-vida-eterna.blogspot.com/" },
+ {"title": "2 ⛹️🚴 BEM ESTAR & SAÚDE 🏋️", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com/" }
 ];
