fab8a9e5392b9a3a6ac5c319fcd7aba41e312174
fabiocruise-cloud <fabiocruise@gmail.com>
2025-11-23 21:15:19 -0300

    criação menus aninhados menus e submenus

--- menu.js
+++ menu.js
@@ -1,7 +1,29 @@
 window.BLOGGER_CENTRAL_MENU = [
- {"title": "Fábio Cruise - Áreas da Vida - Principal", "url": "https://areas-da-vida.blogspot.com/" },
- {"title": "1 🙏🛐😇 FÉ ORAÇÃO VIDA ETERNA", "url": "https://fe-oracao-e-vida-eterna.blogspot.com/" },
- {"title": "2 ⛹️🚴 BEM ESTAR & SAÚDE 🏋️", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com/" },
- {"title": "3 👨‍💼💼 CARREIRA+PROFISSÃO", "url": "https://fabiocruise-trabalho.blogspot.com/" },
- {"title": "4 💲💳 FINANÇAS & INVESTIMENTOS", "url": "https://investimentos-financeiros.blogspot.com/" }
+  {
+    "title": "Fábio Cruise - Áreas da Vida - Principal",
+    "url": "https://areas-da-vida.blogspot.com/"
+  },
+  {
+    "title": "Categorias",
+    "url": "#",
+    "children": [
+      { "title": "FÉ ORAÇÃO VIDA ETERNA", "url": "https://fe-oracao-e-vida-eterna.blogspot.com/" },
+      {
+        "title": "Saúde e Bem-estar",
+        "url": "#",
+        "children": [
+          { "title": "Exercícios", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com/" },
+          { "title": "Nutrição", "url": "https://exemplo-nutricao.blogspot.com/" }
+        ]
+      }
+    ]
+  },
+  {
+    "title": "Carreira",
+    "url": "https://fabiocruise-trabalho.blogspot.com/"
+  },
+  {
+    "title": "Finanças",
+    "url": "https://investimentos-financeiros.blogspot.com/"
+  }
 ];
