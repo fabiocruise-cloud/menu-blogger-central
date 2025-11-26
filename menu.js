a82c24cdfac0185d99c7900304466406812a0190
fabiocruise-cloud <fabiocruise@gmail.com>
2025-11-26 01:52:27 -0300

    Update Nutrition URL in menu.js

--- menu.js
+++ menu.js
@@ -4,11 +4,12 @@ window.BLOGGER_CENTRAL_MENU = [
       { "title": "FÉ ORAÇÃO VIDA ETERNA", "url": "https://fe-oracao-e-vida-eterna.blogspot.com/" },
       { "title": "Saúde e Bem-estar", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com", "children": [ 
         { "title": "Exercícios", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com/" }, 
-        { "title": "Nutrição", "url": "https://exemplo-nutricao.blogspot.com/" },
+        { "title": "Nutrição", "url": "https://investimentos-financeiros.blogspot.com" },
         { "title": "Fitness Avançado", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com", 
          "children": [
-          { "title": "Treino A", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com" },
-          { "title": "Treino B", "url": "#" } ] } ] },
+          { "title": "Treino A", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com/" },
+          { "title": "Treino B", "url": "#" } 
+         ] } ] },
     { "title": "Carreira e Trabalho", "url": "https://fabiocruise-trabalho.blogspot.com/" } ] },
   { "title": "Finanças", "url": "https://investimentos-financeiros.blogspot.com/" },
   { "title": "Sobre", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com", "children": [
