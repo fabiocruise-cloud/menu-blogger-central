0472bb9f3e699795812140d352d50320f9bca666
fabiocruise-cloud <fabiocruise@gmail.com>
2025-11-26 02:23:23 -0300

    Update menu item title to 'Novo Inicio'

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
 window.BLOGGER_CENTRAL_MENU = [
-  { "title": "Fábio Cruise - Áreas da Vida - Principal🧬⏳", "url": "https://areas-da-vida.blogspot.com/" },
+  { "title": "Novo Inicio", "url": "https://areas-da-vida.blogspot.com/" },
   { "title": "Categorias", "url": "#", "children": [
       { "title": "1 🙏🛐😇 FÉ ORAÇÃO VIDA ETERNA", "url": "https://fe-oracao-e-vida-eterna.blogspot.com/" },
       { "title": "2 ⛹️🚴 BEM ESTAR & SAÚDE 🏋️", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com", "children": [ 
