87a394547290d9279ba65a724d7738dc7016587a
fabiocruise-cloud <fabiocruise@gmail.com>
2025-11-27 01:40:24 -0300

    Update URLs in menu.js for various sections

--- menu.js
+++ menu.js
@@ -4,15 +4,13 @@ window.BLOGGER_CENTRAL_MENU = [
       { "title": "2 ⛹️🚴 BEM ESTAR & SAÚDE 🏋️", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com/\" }", "children": [
         { "title": "3 👨‍💼💼 CARREIRA+PROFISSÃO", "url": "https://fabiocruise-trabalho.blogspot.com/\" }", "children": [
           { "title": "4 💲💳 FINANÇAS & INVESTIMENTOS", "url": "https://www.claro.com" },
-          { "title": "5 💡 CONHECIMENTOS GERAIS ANOTAÇÕES-🚀 Cursos", "url": "https://www.uol.com.br" }
+          { "title": "5 💡 CONHECIMENTOS GERAIS ANOTAÇÕES-🚀 Cursos", "url": "https://www.pazevida.org.br" }
         ] }
       ] }
     ] }
   ] },
-  { "title": "6 👨‍👩‍👧‍👦💕 FAMÍLIA & PESSOAL & AGENDAS", "url": "#", "children": [
-    { "title": "7 ✏️📗🕓 MEU GTD - PLANEJAMENTOS & IDÉIAS", "url": "#" },
-    { "title": "8 MEUS TESTES", "url": "https://uol.blogspot.com/\" }" }
+  { "title": "6 👨‍👩‍👧‍👦💕 FAMÍLIA & PESSOAL & AGENDAS", "url": "https://www.g1.com", "children": [
+    { "title": "7 ✏️📗🕓 MEU GTD - PLANEJAMENTOS & IDÉIAS", "url": "https://www.ge.com" },
+    { "title": "8 MEUS TESTES", "url": "https://www.uol.com.br" }
   ] }
 ];
-
-
