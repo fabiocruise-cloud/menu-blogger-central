84e9113a0bf3c6a6a11f56bb0a986956f7f945d4
fabiocruise-cloud <fabiocruise@gmail.com>
2025-11-27 01:58:59 -0300

    Update URLs in the menu structure

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ window.BLOGGER_CENTRAL_MENU = [
   { "title": "Fábio Cruise - Áreas da Vida - Principal🧬⏳", "url": "https://areas-da-vida.blogspot.com/\" }", "children": [
     { "title": "1 🙏🛐😇 FÉ ORAÇÃO VIDA ETERNA", "url": "https://fe-oracao-e-vida-eterna.blogspot.com/\" }", "children": [
       { "title": "2 ⛹️🚴 BEM ESTAR & SAÚDE 🏋️", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com/\" }", "children": [
-        { "title": "3 👨‍💼💼 CARREIRA+PROFISSÃO", "url": "https://fabiocruise-trabalho.blogspot.com/\" }", "children": [
+        { "title": "3 👨‍💼💼 CARREIRA+PROFISSÃO", "url": "https://www.facebook.com", "children": [
           { "title": "4 💲💳 FINANÇAS & INVESTIMENTOS", "url": "https://www.claro.com" },
           { "title": "5 💡 CONHECIMENTOS GERAIS ANOTAÇÕES-🚀 Cursos", "url": "https://www.pazevida.org.br" }
         ] }
@@ -11,6 +11,6 @@ window.BLOGGER_CENTRAL_MENU = [
   ] },
   { "title": "6 👨‍👩‍👧‍👦💕 FAMÍLIA & PESSOAL & AGENDAS", "url": "https://www.g1.com", "children": [
     { "title": "7 ✏️📗🕓 MEU GTD - PLANEJAMENTOS & IDÉIAS", "url": "https://www.ge.com" },
-    { "title": "8 MEUS TESTES", "url": "https://www.uol.com.br" }
+    { "title": "8 MEUS TESTES", "url": "https://www.youtube.com/" }
   ] }
 ];
