c0e729363c8215917af8ea136ca0fbd79afe6c77
fabiocruise-cloud <fabiocruise@gmail.com>
2025-11-27 00:18:18 -0300

    Remove commented Java code from menu.js

--- menu.js
+++ menu.js
@@ -1,5 +1,3 @@
-/* ⬇️CÓDIGO JAVA⬇️ */
-
 window.BLOGGER_CENTRAL_MENU = [
   { "title": "Fábio Cruise - Áreas da Vida - Principal🧬⏳", "url": "https://areas-da-vida.blogspot.com/", "children": [
     { "title": "1 🙏🛐😇 FÉ ORAÇÃO VIDA ETERNA", "url": "https://fe-oracao-e-vida-eterna.blogspot.com/", "children": [
