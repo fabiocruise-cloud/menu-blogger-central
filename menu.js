57ebea592d31fb2e98d9b06f8f7e6080422baba9
fabiocruise-cloud <fabiocruise@gmail.com>
2025-11-27 00:45:03 -0300

    Update menu links and titles in menu.js

--- menu.js
+++ menu.js
@@ -1,17 +1,18 @@
 /* ⬇️CÓDIGO JAVA⬇️ */
+
 window.BLOGGER_CENTRAL_MENU = [
-  { "title": "Fábio Cruise - Áreas da Vida - Principal🧬⏳", "url": "https://areas-da-vida.blogspot.com/", "children": [
-    { "title": "1 🙏🛐😇 FÉ ORAÇÃO VIDA ETERNA", "url": "https://fe-oracao-e-vida-eterna.blogspot.com/", "children": [
-      { "title": "2 ⛹️🚴 BEM ESTAR & SAÚDE 🏋️", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com/", "children": [
-        { "title": "3 👨‍💼💼 CARREIRA+PROFISSÃO", "url": "https://fabiocruise-trabalho.blogspot.com/", "children": [
-          { "title": "4 💲💳 FINANÇAS & INVESTIMENTOS", "url": "https://investimentos-financeiros.blogspot.com/" },
-          { "title": "5 💡 CONHECIMENTOS GERAIS ANOTAÇÕES-🚀 Cursos", "url": "#" }
+  { "title": "Fábio Cruise - Áreas da Vida - Principal🧬⏳", "url": "https://areas-da-vida.blogspot.com/\" }", "children": [
+    { "title": "1 🙏🛐😇 FÉ ORAÇÃO VIDA ETERNA", "url": "https://fe-oracao-e-vida-eterna.blogspot.com/\" }", "children": [
+      { "title": "2 ⛹️🚴 BEM ESTAR & SAÚDE 🏋️", "url": "https://fabiocruise-bem-estar-e-saude.blogspot.com/\" }", "children": [
+        { "title": "3 👨‍💼💼 CARREIRA+PROFISSÃO", "url": "https://fabiocruise-trabalho.blogspot.com/\" }", "children": [
+          { "title": "4 💲💳 FINANÇAS & INVESTIMENTOS", "url": "https://www.g1.com.br" },
+          { "title": "5 💡 CONHECIMENTOS GERAIS ANOTAÇÕES-🚀 Cursos", "url": "https://www.pazevida.org" }
         ] }
       ] }
     ] }
   ] },
-  { "title": "6 👨‍👩‍👧‍👦💕 FAMÍLIA & PESSOAL & AGENDAS", "url": "#", "children": [
-    { "title": "7 ✏️📗🕓 MEU GTD - PLANEJAMENTOS & IDÉIAS", "url": "#" },
-    { "title": "8 MEUS TESTES", "url": "https://uol.blogspot.com/" }
+  { "title": "6 👨‍👩‍👧‍👦💕 FAMÍLIA & PESSOAL & AGENDAS", "url": "https://www.globo.com", "children": [
+    { "title": "7 ✏️📗🕓 MEU GTD - PLANEJAMENTOS & IDÉIAS", "url": "https://www.claro.com" },
+    { "title": "8 MEUS TESTES", "url": "https://www.uol.com.br" }
   ] }
 ];
